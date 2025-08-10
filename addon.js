8559628f20025b193fdb0ba0ececdd9ef11392ad
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-08-09 22:38:24 -0500

    Fixing description typos

--- addon.js
+++ addon.js
@@ -194,11 +194,11 @@ app.get('/:config/stream/:type/:id.json', async (req, res) => {
             streams: directUrl ? [{
                 name: 'YT-DLP Player (MP4)',
                 url: directUrl,
-                description: 'Click to watch the scraped MP3 from YT-DLP'
+                description: 'Click to watch the scraped MP4 from YT-DLP',
             }, {
                 name: 'YT-DLP Player (Non-MP4)',
                 url: directUrl,
-                description: 'Click to watch the scraped MP3 from YT-DLP',
+                description: 'Click to watch the scraped Non-MP4 from YT-DLP',
                 notWebReady: true
             }, {
                 name: 'Stremio Player',
