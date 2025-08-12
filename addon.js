68bcb95de1ae20ed2c6b7d23c7c2be58cbaa4fb4
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-08-11 23:22:49 -0500

    Update deprecated encryption

--- addon.js
+++ addon.js
@@ -18,14 +18,13 @@ const ALGORITHM = 'aes-256-gcm';
 
 function encrypt(text) {
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipher(ALGORITHM, ENCRYPTION_KEY);
-    cipher.setIV(iv);
+    const cipher = crypto.createCipheriv(ALGORITHM, ENCRYPTION_KEY, iv);
     
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     
     const authTag = cipher.getAuthTag();
-    
+
     return iv.toString('hex') + ':' + authTag.toString('hex') + ':' + encrypted;
 }
 
@@ -39,15 +38,15 @@ function decrypt(encryptedData) {
     const authTag = Buffer.from(parts[1], 'hex');
     const encrypted = parts[2];
     
-    const decipher = crypto.createDecipher(ALGORITHM, ENCRYPTION_KEY);
-    decipher.setIV(iv);
+    const decipher = crypto.createDecipheriv(ALGORITHM, ENCRYPTION_KEY, iv);
     decipher.setAuthTag(authTag);
-    
+
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     
     return decrypted;
 }
+
 
 const manifest = {
     id: 'youtubio.elfhosted.com',
