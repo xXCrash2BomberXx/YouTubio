d4637f564f68cc585b537b6316a37587b990d71b
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-08-19 19:04:05 -0500

    More flags

--- addon.js
+++ addon.js
@@ -212,9 +212,9 @@ app.get('/:config/meta/:type/:id.json', async (req, res) => {
 
     const video = await runYtDlpWithAuth(req.params.config, [
         `https://www.youtube.com/${channel ? '' : 'watch?v='}${videoId}`,
-        ...(channel ? ['--flat-playlist', '--dump-single-json'] : [
-            '-j',
-            ...(req.params.config.markWatchedOnLoad ? ['--mark-watched'] : [])])]);
+        '-j',
+        ...(channel ? ['--flat-playlist', '--dump-single-json'] : []),
+        ...(!channel && req.params.config.markWatchedOnLoad ? ['--mark-watched'] : [])]);
     const title = video.title ?? 'Unknown Title';
     const thumbnail = `${channel ? protocol + ':' : ''}${video.thumbnail ?? video.thumbnails?.at(-1)?.url}`;
     const released = video.timestamp ? new Date(video.timestamp * 1000).toISOString() : undefined;
