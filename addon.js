1373143d9f68128d80d4ee606f5a30ebe34efa59
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-08-20 10:41:12 -0500

    Remove explicit format

--- addon.js
+++ addon.js
@@ -213,7 +213,7 @@ app.get('/:config/meta/:type/:id.json', async (req, res) => {
 
     const video = await runYtDlpWithAuth(req.params.config, [
         `https://www.youtube.com/${channel ? '' : 'watch?v='}${videoId}`,
-        ...(channel ? ['-J', '--flat-playlist'] : ['-j', '-f', 'bv*+ba/best']),
+        ...(channel ? ['-J', '--flat-playlist'] : ['-j']),
         ...(!channel && req.params.config.markWatchedOnLoad ? ['--mark-watched'] : [])]);
     const title = video.title ?? 'Unknown Title';
     const thumbnail = `${channel ? protocol + ':' : ''}${video.thumbnail ?? video.thumbnails?.at(-1)?.url}`;
