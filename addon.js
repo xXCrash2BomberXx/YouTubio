ef0d050c2a324efa846200d8bfd82dea3dbd8188
xxcrash2bomberxx <bleesman21@gmail.com>
2025-08-21 17:23:55 -0500

    Remove dev route

--- addon.js
+++ addon.js
@@ -71,13 +71,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 // Config Encryption Endpoint
 app.post('/encrypt', (req, res) => {
     try {
