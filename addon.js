589946318d01fc6fc02655b928dd0bf77e729950
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-08-25 15:19:15 -0500

    CORs & HTTPS fixes

--- addon.js
+++ addon.js
@@ -73,8 +73,20 @@ async function runYtDlpWithAuth(encryptedConfig, argsArray) {
 }
 
 const app = express();
+app.set('trust proxy', true);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
 
 // Config Encryption Endpoint
 app.post('/encrypt', (req, res) => {
