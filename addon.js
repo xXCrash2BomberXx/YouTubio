4931e498de0a0250ee51d559a6612fcedc8e96c0
xxcrash2bomberxx <bleesman21@gmail.com>
2025-08-27 09:45:14 -0500

    Fix Year Loading

--- addon.js
+++ addon.js
@@ -229,7 +229,7 @@ app.get('/:config/meta/:type/:id.json', async (req, res) => {
         const channel = video._type === 'playlist';
         const title = video.title ?? 'Unknown Title';
         const thumbnail = video.thumbnail ?? video.thumbnails?.at(-1).url;
-        const released = new Date((video.release_timestamp ?? 0) * 1000).toISOString();
+        const released = new Date(video.release_timestamp ? video.release_timestamp * 1000 : video.upload_date ? `${video.upload_date.substring(0, 4)}-${video.upload_date.substring(4, 6)}-${video.upload_date.substring(6, 8)}T00:00:00` : 0).toISOString();
         const subtitles = Object.entries(video.subtitles ?? {}).map(([k, v]) => {
             const srt = v.find?.(x => x.ext == 'srt') ?? v[0];
             return srt ? {
@@ -256,7 +256,7 @@ app.get('/:config/meta/:type/:id.json', async (req, res) => {
             posterShape: channel ? 'square' : 'landscape',
             background: thumbnail,
             description: video.description,
-            releaseInfo: video.release_year,
+            releaseInfo: video.release_year ?? video.upload_date?.substring(0, 4),
             released: released,
             videos: [{
                 id: req.params.id,
