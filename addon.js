26be90c3e06f2f75b86e8e9520ce2c2fca38103d
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-09-08 11:32:57 -0500

    Cleanup

--- addon.js
+++ addon.js
@@ -993,6 +993,3 @@ app.listen(PORT, () => {
     }
     console.log(`Access the configuration page at: ${process.env.SPACE_HOST ? 'https://' + process.env.SPACE_HOST : 'http://localhost:' + PORT}`);
 });
-
-
-
