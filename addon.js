23fa299b505c7196633705c95671984b6ec5a4be
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2025-11-08 15:36:22 -0600

    Formatting

--- addon.js
+++ addon.js
@@ -1444,4 +1444,3 @@ app.listen(PORT, () => {
     }
     console.log(`Access the configuration page at: ${process.env.SPACE_HOST ? 'https://' + process.env.SPACE_HOST : 'http://localhost:' + PORT}`);
 });
-
